5cd9b57b38cbed8bc224f232015a9b63d8fd196b
danimaky <daniel.nunez.ext@nokia.com>
2019-03-22 09:09:46 -0500

    commented solution without concurrency and time calculator

--- queens.py
+++ queens.py
@@ -20,26 +20,26 @@ MOVEMENTS = {
 }
 
 
-def calculate(value, operator):
-    if OPERATORS[operator]:
-        value = OPERATORS[operator](value, 1)
+def calculate(value, op):
+    if OPERATORS[op]:
+        value = OPERATORS[op](value, 1)
     return value
 
 
-def movementscounter(dst, length, queen, obstacles_positions):
-    movements_counter = 0
+def movements_counter(dst, length, queen, obstacles_positions):
+    counter = 0
     pointer = queen
     pointer = [calculate(pointer[0], MOVEMENTS[dst][0]), calculate(pointer[1], MOVEMENTS[dst][1])]
     while 0 < pointer[0] <= length and 0 < pointer[1] <= length:
         if pointer in obstacles_positions:
             break
         else:
-            movements_counter += 1
+            counter += 1
             pointer = [calculate(pointer[0], MOVEMENTS[dst][0]), calculate(pointer[1], MOVEMENTS[dst][1])]
-    return movements_counter
+    return counter
 
 
-def queensattack():
+def queens_attack():
     with open("game4.txt") as file:
         input_txt = [list(map(int, a.split(' '))) for a in file.read().split('\n')]
     length = input_txt[0][0]
@@ -49,17 +49,20 @@ def queensattack():
     if len(obstacles_positions) != obstacles_quantity or 0 > length > 100000 and queen in obstacles_positions:
         print("Validation error!")
     else:
-        counter = 0
-        for movement in MOVEMENTS.keys():
-            counter = counter + movementscounter(movement, length, queen, obstacles_positions)
-        print(counter)
-        # with Pool(processes=4) as pool:
-        #     multiple_results = [pool.apply_async(movementscounter, (movement, length, queen, obstacles_positions)) for movement in MOVEMENTS.keys()]
-        #     print(sum([res.get() for res in multiple_results]))
+        # Solution without threading
+        # counter = 0
+        # for movement in MOVEMENTS.keys():
+        #     counter = counter + movements_counter(movement, length, queen, obstacles_positions)
+        # print(counter)
+
+        # Solution with threading
+        with Pool(processes=4) as pool:
+            multiple_results = [pool.apply_async(movements_counter, (movement, length, queen, obstacles_positions)) for movement in MOVEMENTS.keys()]
+            print(sum([res.get() for res in multiple_results]))
 
 
 if __name__ == '__main__':
-    tic = time.clock()
-    queensattack()
-    toc = time.clock()
-    print(toc - tic)
+    # tic = time.clock()
+    queens_attack()
+    # toc = time.clock()
+    # print(toc - tic)
