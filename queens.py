f48ce7b31f7b6a3870682a29cb5c0f1c24dc643c
Daniel Nunez <danielsistem.ing96@gmail.com>
2019-03-21 21:36:24 -0500

    Refactor to solution

--- queens.py
+++ queens.py
@@ -10,7 +10,7 @@ MOVEMENTS = {
     "UP": ("0", "+"),
     "UP-RIGHT": ("+", "+"),
     "RIGHT": ("+", "0"),
-    "DOWN-RIGHT": ("+" ,"-"),
+    "DOWN-RIGHT": ("+", "-"),
     "DOWN": ("0", "-"),
     "DOWN-LEFT": ("-", "-"),
     "LEFT": ("-", "0"),
@@ -20,15 +20,15 @@ MOVEMENTS = {
 
 def calculate(value, operator):
     if OPERATORS[operator]:
-        value = OPERATORS[operator](int(value), 1)
-    return str(value)
+        value = OPERATORS[operator](value, 1)
+    return value
 
 
 def movementscounter(dst, length, queen, obstacles_positions):
     movements_counter = 0
     pointer = queen
     pointer = [calculate(pointer[0], MOVEMENTS[dst][0]), calculate(pointer[1], MOVEMENTS[dst][1])]
-    while int(pointer[0]) <= length and int(pointer[0]) > 0 and int(pointer[1]) <= length and int(pointer[1]) > 0:
+    while 0 < pointer[0] <= length and 0 < pointer[1] <= length:
         try:
             obstacles_positions.index(pointer)
             break
@@ -40,15 +40,18 @@ def movementscounter(dst, length, queen, obstacles_positions):
 
 def queensattack():
     with open("game4.txt") as file:
-        input_txt = [a.split(' ') for a in file.read().split('\n')]
-    length = int(input_txt[0][0])
+        input_txt = [list(map(int, a.split(' '))) for a in file.read().split('\n')]
+    length = input_txt[0][0]
     obstacles_quantity = input_txt[0][1]
     queen = input_txt[1]
     obstacles_positions = input_txt[2:]
+    if len(obstacles_positions) != obstacles_quantity or 0 > length > 100000:
+        print("Validation error!")
     counter = 0
     for movement in MOVEMENTS.keys():
         counter = counter + movementscounter(movement, length, queen, obstacles_positions)
     print(counter)
 
 
-queensattack()
+if __name__ == '__main__':
+    queensattack()
