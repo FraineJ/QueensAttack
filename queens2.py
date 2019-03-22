d27742ad2770df5ae36f6555ab5a97445b909f3d
Daniel Nunez <danielsistem.ing96@gmail.com>
2019-03-21 23:05:51 -0500

    Created a new way to solve this

--- queens2.py
+++ queens2.py
@@ -1,6 +1,12 @@
 import operator
 
 OPERATORS = {
+    "+": operator.gt,
+    "-": operator.lt,
+    "0": operator.eq
+}
+
+OPERATORS_FOR_CALCULATE = {
     "+": operator.add,
     "-": operator.sub,
     "0": None
@@ -18,9 +24,21 @@ MOVEMENTS = {
 }
 
 
+def take_second(elem):
+    return elem[1]
+
+
+def take_first(elem):
+    return elem[0]
+
+
+def filterobstaclesbydirection(dir, queen, obstacle):
+    return OPERATORS[MOVEMENTS[dir][0]](obstacle[0], queen[0]) and OPERATORS[MOVEMENTS[dir][1]](obstacle[1], queen[1])
+
+
 def calculate(value, operator):
-    if OPERATORS[operator]:
-        value = OPERATORS[operator](value, 1)
+    if OPERATORS_FOR_CALCULATE[operator]:
+        value = OPERATORS_FOR_CALCULATE[operator](value, 1)
     return value
 
 
@@ -39,18 +57,45 @@ def movementscounter(dst, length, queen, obstacles_positions):
 
 
 def queensattack():
-    with open("game4.txt") as file:
-        input_txt = [list(map(int, a.split(' '))) for a in file.read().split('\n')]
+    with open("game.txt") as file:
+        input_txt = [tuple(map(int, a.split(' '))) for a in file.read().split('\n')]
     length = input_txt[0][0]
     obstacles_quantity = input_txt[0][1]
     queen = input_txt[1]
-    obstacles_positions = input_txt[2:]
-    if len(obstacles_positions) != obstacles_quantity or 0 > length > 100000:
+    obstacles_positions = tuple(input_txt[2:])
+    if len(obstacles_positions) != obstacles_quantity or 0 > length > 100000 and queen in obstacles_positions:
         print("Validation error!")
-    counter = 0
-    for movement in MOVEMENTS.keys():
-        counter = counter + movementscounter(movement, length, queen, obstacles_positions)
-    print(counter)
+    else:
+        counter = 0
+        for movement in MOVEMENTS.keys():
+            if movement in ("UP", "RIGHT", "DOWN", "LEFT"):
+                obstacles = list(filter(lambda x: filterobstaclesbydirection(movement, queen, x), obstacles_positions))
+                if len(obstacles) == 0:
+                    if movement == "UP":
+                        counter = counter + (length - queen[1])
+                    elif movement == "RIGHT":
+                        counter = counter + (length - queen[0])
+                    elif movement == "DOWN":
+                        counter = counter + (queen[1] - 1)
+                    elif movement == "LEFT":
+                        counter = counter + (queen[0] - 1)
+                else:
+                    if movement == "UP":
+                        obstacles.sort(key=take_second)
+                        counter = counter + (obstacles[0][1] - queen[1])
+                    elif movement == "RIGHT":
+                        obstacles.sort(key=take_first)
+                        counter = counter + (length - queen[0])
+                    elif movement == "DOWN":
+                        obstacles.sort(key=take_second, reverse=True)
+                        counter = counter + (queen[1] - obstacles[0][1])
+                    elif movement == "LEFT":
+                        obstacles.sort(key=take_first, reverse=True)
+                        counter = counter + (queen[0] - obstacles[0][0])
+                    counter -= 1
+            else:
+                counter = counter + movementscounter(movement, length, queen, obstacles_positions)
+        print(counter)
 
 
 if __name__ == '__main__':
